b648a4c27b2f6a7328cf91f17923a2b578118885
pglez82 <pglez82@gmail.com>
2023-06-05 11:52:28 +0200

    update how to return sample metadata

--- h5ifcbdataset.py
+++ h5ifcbdataset.py
@@ -2,7 +2,7 @@ from torch.utils.data import Dataset
 import torch
 import h5py
 import numpy as np
-import io,sys
+import io, sys
 from PIL import Image
 from pathlib import Path
 from tqdm import tqdm
@@ -10,28 +10,38 @@ import pandas as pd
 
 
 class H5IFCBDataset(Dataset):
-    def __init__(self, files, classes,classattribute,verbose=0,trainingset=True,defaultclass="mix", metadata_file=None, transform=None):
-        '''
+    def __init__(
+        self,
+        files,
+        classes,
+        classattribute,
+        verbose=0,
+        trainingset=True,
+        defaultclass="mix",
+        metadata_file=None,
+        transform=None,
+    ):
+        """
         Inits the dataset
         :param list files: List of HDF5 files to load. This list can be empty if you plan to load the dataset from disk later.
         :param list classes: Sorted list with the classes of the problem as strings (must be coherent with the next attribute)
         :param str classattribute: Attribute in the hdf5 files that contains the class. For IFCB can be: AutoClass, OriginalClass or FunctionalGroup
         :param int verbose: Show information of the process
         :param transform: transformations. See test.py for an example. At least the transformation to Tensor should be there
-        :return: 
-        '''
+        :return:
+        """
         self.verbose = verbose
         self.transform = transform
-        if (type(classes) is np.ndarray and (classes != np.sort(classes)).any()):
+        if type(classes) is np.ndarray and (classes != np.sort(classes)).any():
             raise ValueError("classes attribute should be a numpy array sorted")
         self.classes = classes
         self.classes.sort()
         self.files = files
-        self.files.sort() #Load them in order so we get always the same results
+        self.files.sort()  # Load them in order so we get always the same results
         self.class_to_idx = {cls_name: i for i, cls_name in enumerate(self.classes)}
-        self.samples_idx = {} #Map that relates sample name with example indexes
+        self.samples_idx = {}  # Map that relates sample name with example indexes
 
-        #We use lists because we do not know the sizes until we finish loading. This is the most efficent way
+        # We use lists because we do not know the sizes until we finish loading. This is the most efficent way
         self.targets = []
         self.images = []
         self.samples = []
@@ -39,50 +49,58 @@ class H5IFCBDataset(Dataset):
         if metadata_file is not None:
             self.metadata = pd.read_csv(metadata_file, index_col=0)
 
-        #Open the files and store the images into memory
+        # Open the files and store the images into memory
         example_index = 0
-        for i in tqdm(range(len(files)),desc='Loading samples: ',disable=(self.verbose<1)):
+        sample_names = []
+        for i in tqdm(range(len(files)), desc="Loading samples: ", disable=(self.verbose < 1)):
             file = files[i]
-            f = h5py.File(file, 'r')
+            f = h5py.File(file, "r")
             sample = Path(file).stem
+            sample_names.append(sample)
             for example in f.keys():
                 cl = f[example].attrs[classattribute]
-                #What if the examples have a class that is not in the list of classes given
+                # What if the examples have a class that is not in the list of classes given
                 if cl not in self.class_to_idx:
                     self.targets.append(self.class_to_idx[defaultclass])
                 else:
                     self.targets.append(self.class_to_idx[cl])
                 self.images.append(np.array(f[example]))
                 self.samples.append(sample)
-            
-            self.samples_idx[sample] = np.array(range(example_index,example_index+len(f)))
-            example_index+=len(f)
+
+            self.samples_idx[sample] = np.array(range(example_index, example_index + len(f)))
+            example_index += len(f)
 
             f.close()
-        #Check that we have examples of all classes if we are in a training set
+        if metadata_file is not None:
+            self.metadata = self.metadata.loc[sample_names]
+        # Check that we have examples of all classes if we are in a training set
         if trainingset and files:
-            real_classes=np.unique(self.targets)
+            real_classes = np.unique(self.targets)
             real_classes.sort()
-            missing_classes = np.setdiff1d(np.arange(len(classes)),real_classes)
-            if missing_classes.size>0:
-                print("ERROR. YOU MUST REBUILD THE DATASET! Not all the classes are in the training set: {}. Remove them from the classes list and start again.".format(missing_classes))
-    
+            missing_classes = np.setdiff1d(np.arange(len(classes)), real_classes)
+            if missing_classes.size > 0:
+                print(
+                    "ERROR. YOU MUST REBUILD THE DATASET! Not all the classes are in the training set: {}. Remove them from the classes list and start again.".format(
+                        missing_classes
+                    )
+                )
+
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, index):
         img = Image.open(io.BytesIO(self.images[index]))
-        img = img.convert('RGB')
+        img = img.convert("RGB")
         target = int(self.targets[index])
         sample = self.samples[index]
         if self.transform is not None:
             img = self.transform(img)
-        return img,target,sample
-    
-    def get_sample_metadata(self, sample):
+        return img, target, sample
+
+    def get_sample_metadata(self, samples):
         if self.metadata is None:
             raise ValueError("Metadata is none, have you passed the parameter metadata_file in the constructor?")
-        return self.metadata.loc[sample].to_numpy()
+        return self.metadata.loc[samples].to_numpy()
 
     def get_metadata_size(self):
         if self.metadata is None:
@@ -94,9 +112,26 @@ class H5IFCBDataset(Dataset):
         Saves the loaded dataset to disk in just one file. This can be faster than loading a file per sample.
         """
         if self.metadata is not None:
-            torch.save({"images": self.images, "targets":self.targets, "samples":self.samples, "samples_idx":self.samples_idx, "metadata": self.metadata}, file)
+            torch.save(
+                {
+                    "images": self.images,
+                    "targets": self.targets,
+                    "samples": self.samples,
+                    "samples_idx": self.samples_idx,
+                    "metadata": self.metadata,
+                },
+                file,
+            )
         else:
-            torch.save({"images": self.images, "targets":self.targets, "samples":self.samples, "samples_idx":self.samples_idx}, file)
+            torch.save(
+                {
+                    "images": self.images,
+                    "targets": self.targets,
+                    "samples": self.samples,
+                    "samples_idx": self.samples_idx,
+                },
+                file,
+            )
 
     def load(self, file):
         """
@@ -104,9 +139,9 @@ class H5IFCBDataset(Dataset):
         call this method.
         """
         data = torch.load(file)
-        self.images = data['images']
-        self.targets = data['targets']
-        self.samples = data['samples']
-        self.samples_idx = data['samples_idx']
-        if 'metadata' in data:
-            self.metadata = data['metadata']
+        self.images = data["images"]
+        self.targets = data["targets"]
+        self.samples = data["samples"]
+        self.samples_idx = data["samples_idx"]
+        if "metadata" in data:
+            self.metadata = data["metadata"]
